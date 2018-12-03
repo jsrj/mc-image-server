d579c69614a816bd9cc368805abf64353dea64ab
jsrj <rj.javadev@icloud.com>
2018-12-02 20:26:55 -0500

    Stubbed out basic Image Response DTO

--- src/main/java/com/arjaycodes/makercloudimageserver/projection/ImageResponseDTO.java
+++ src/main/java/com/arjaycodes/makercloudimageserver/projection/ImageResponseDTO.java
@@ -1,4 +1,27 @@
 package com.arjaycodes.makercloudimageserver.projection;
 
+import com.arjaycodes.makercloudimageserver.model.ImageData;
+import com.arjaycodes.makercloudimageserver.model.utility.PageInfo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class ImageResponseDTO {
+
+    @Getter @Setter
+    private PageInfo pageInfo;
+
+    @Getter
+    private final List<ImageData> data = new ArrayList<>();
 }
