3ff5e83de09aa10a563f6a1b60ee523bbb77e9ab
jsrj <rj.javadev@icloud.com>
2018-12-04 21:24:01 -0500

    Renamed StandardResponseDTO

--- src/main/java/com/arjaycodes/makercloudimageserver/service/ImageUpdateAndRemovalService.java
+++ src/main/java/com/arjaycodes/makercloudimageserver/service/ImageUpdateAndRemovalService.java
@@ -1,12 +1,12 @@
 package com.arjaycodes.makercloudimageserver.service;
 
-import com.arjaycodes.makercloudimageserver.projection.StandardResponseDTO;
+import com.arjaycodes.makercloudimageserver.projection.StandardResponse;
 
 public interface ImageUpdateAndRemovalService {
 
     // Updates actual Base-64 Image Encoding by a given ID
-    StandardResponseDTO updateImageData (Long imageId);
+    StandardResponse updateImageData (Long imageId);
 
     // Removes/Deletes an Image Entity from Datastore by a given ID
-    StandardResponseDTO deleteImageEntity (Long imageId);
+    StandardResponse deleteImageEntity (Long imageId);
 }
