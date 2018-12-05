c9963c8d7caeccdee718c34371c84611f2d89591
jsrj <rj.javadev@icloud.com>
2018-12-04 21:23:10 -0500

    Added basic service method outlines to ImageUploadService interface

--- src/main/java/com/arjaycodes/makercloudimageserver/service/ImageUploadService.java
+++ src/main/java/com/arjaycodes/makercloudimageserver/service/ImageUploadService.java
@@ -1,6 +1,11 @@
 package com.arjaycodes.makercloudimageserver.service;
 
+import com.arjaycodes.makercloudimageserver.projection.StandardResponse;
+
 public interface ImageUploadService {
     // Adds a new Image Entity by owner
+    StandardResponse addImageByOwner(String ownerName);
+
     // Adds multiple Image Entities by Owner
+    StandardResponse addMultipleImagesByOwner(String ownerName);
 }
