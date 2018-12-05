cb539fd1ff315a5bdc77b2856cdbe7e6fdbb2850
jsrj <rj.javadev@icloud.com>
2018-12-05 00:31:46 -0500

    Disabled Spring Security autoconfiguration

--- src/main/java/com/arjaycodes/makercloudimageserver/MakercloudImageServerApplication.java
+++ src/main/java/com/arjaycodes/makercloudimageserver/MakercloudImageServerApplication.java
@@ -2,10 +2,10 @@ package com.arjaycodes.makercloudimageserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class MakercloudImageServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MakercloudImageServerApplication.class, args);
     }
